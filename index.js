4a0a62a61e523a48af3e9346f51132051c4aa457
Alberto Blanco <itsnohax@gmail.com>
2018-08-30 23:16:08 +0200

    Set up store persisting and rehydration
    We wrapped persistStore function in a promise so that we can wait for it to be ready amonst other things before we initialize the layout

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Navigation } from 'react-native-navigation';
+import { persistStore as persistStoreRaw } from 'redux-persist';
 import store from './src/store/store';
 import { registerScreens } from './src/screens/screens';
 import { setInitialLayout } from './src/NavigationController';
@@ -13,12 +14,31 @@ const registerNavigationListener = () => new Promise((resolve) => {
   });
 });
 
+/**
+ * Wait till our store is persisted
+ * @param {store} storeToPersist - The redux store to persist
+ * @returns {Promise} - Promise that resolves when the store is rehydrated
+ */
+const persistStore = storeToPersist => new Promise((resolve) => {
+  persistStoreRaw(storeToPersist, undefined, () => {
+    resolve();
+  });
+});
 
+/**
+ * We register screens
+ *  then we wait for
+ *    - Navigation to be ready
+ *    - Store to be rehydrated
+ *    - Icons to be loaded.
+ * and then we finally initialize
+ * layout accordingly.
+ */
 async function bootstrap() {
   registerScreens(store);
 
-  // TODO: wait for Redux-Persist + Vector Icons
-  await Promise.all([registerNavigationListener()]);
+  // TODO: wait for Vector Icons
+  await Promise.all([registerNavigationListener(), persistStore(store)]);
 
   setInitialLayout();
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/reducers';
 
 const loggerMiddleware = createLogger();
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore() {
   return createStore(
-    rootReducer,
+    persistedReducer,
     applyMiddleware(
       thunkMiddleware,
       loggerMiddleware,
