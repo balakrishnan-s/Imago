d55c42beced402639a65cdd719dbd1bc02535a03
Alberto Blanco <itsnohax@gmail.com>
2018-11-27 22:44:23 +0100

    Merge the events array

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -5,20 +5,23 @@ const initialState = {
   lastFetch: new Date(),
 };
 
-const events = (state = initialState, action) => {
+function eventsReducer(state = initialState, action) {
   switch (action.type) {
 
-    case RECEIVE_EVENTS:
+    case RECEIVE_EVENTS: {
+      const currentEvents = state.events;
+      const payloadEvents = action.payload.events;
       return {
         ...state,
-        events: action.payload.events,
+        events: [...new Set([...currentEvents, ...payloadEvents])],
         lastFetch: action.payload.receivedAt,
       };
+    }
 
     default:
       return state;
 
   }
-};
+}
 
-export default events;
+export default eventsReducer;
