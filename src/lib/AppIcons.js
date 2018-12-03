9802e877bdc990e07cab76262d79a3ea93bdbe98
Alberto Blanco <itsnohax@gmail.com>
2018-12-03 19:52:44 +0100

    Document loadIcons function

--- src/lib/AppIcons.js
+++ src/lib/AppIcons.js
@@ -2,6 +2,10 @@ import Icon from 'react-native-vector-icons/Feather';
 
 const loadedIconsMap = {};
 
+/**
+ * Load icons into memory for RNN bottomTabs and leftButtons/rightButtons
+ * @returns {Promise} Returns a promise that resolves when all the icons are loaded into memory.
+ */
 async function loadIcons() {
   loadedIconsMap.map = await Icon.getImageSource('map', 24, 'white');
   loadedIconsMap.heart = await Icon.getImageSource('heart', 24, 'white');
