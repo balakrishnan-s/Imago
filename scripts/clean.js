f873b6f8022a68d4b0546e7751d01eacc3fcbe98
Alberto Blanco <itsnohax@gmail.com>
2018-12-29 13:44:07 +0100

    Make clean command Mac friendly

--- scripts/clean.js
+++ scripts/clean.js
@@ -4,7 +4,7 @@ const execCommand = require('./util/execCommand');
 const os = require('os');
 const platform = os.platform();
 
-if (platform.indexOf('win') !== -1) {
+if (platform.indexOf('win') !== -1 && platform.indexOf('darwin') === -1) {
   execCommand('rmdir /s /q node_modules');
   // not sure what other windows clean commands are
 } else {
